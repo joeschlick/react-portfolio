2cc39e335a661b4702cd141c615d0d6faab4312f
slowdive <schlickjoe@gmail.com>
2020-12-08 13:29:50 -0800

    lost typed effect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Avatar, Grid, Box } from "@material-ui/core";
-import Typed from "react-typed";
+
 import coolpic from "../coolpic.jpg";
 import zIndex from "@material-ui/core/styles/zIndex";
 
 //CSS Styles
 const useStyles = makeStyles((theme) => ({
   avatar: {
-    width: theme.spacing(30),
-    height: theme.spacing(30),
+    width: theme.spacing(20),
+    height: theme.spacing(20),
     margin: theme.spacing(1),
   },
   title: {
-    color: "tomato",
+    color: "white",
   },
   subtitle: {
-    color: "tan",
+    color: "white",
     marginBottom: "3rem",
   },
   typedContainer: {
@@ -39,17 +39,12 @@ const Header = () => {
       <Grid container justify="center">
         <Avatar className={classes.avatar} src={coolpic} alt="pic of me" />
       </Grid>
-      <Typography className={classes.title} variant="h4">
-        <Typed strings={["Joe Schlick"]} typeSpeed={40} />
+      <Typography className={classes.title} variant="h3">
+      Joe Schlick
       </Typography>
       <br />
       <Typography className={classes.subtitle} variant="h5">
-        <Typed
-          strings={["Web Design", "Web Development", "MERN Stack"]}
-          typeSpeed={40}
-          backSpeed={60}
-          loop
-        />
+        MERN Stack Web Developer
       </Typography>
     </Box>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -27,7 +27,7 @@ import coolpic from "../coolpic.jpg";
 const useStyles = makeStyles((theme) => ({
   menuSliderContainer: {
     width: 250,
-    background: "#511",
+    background: "black",
     height: "100%",
   },
   avatar: {
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
     height: theme.spacing(13),
   },
   listItem: {
-    color: "tan",
+    color: "white",
   },
 }));
 
