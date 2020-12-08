78876e569b2716b414b0bacfba916af57d8e78e3
slowdive <schlickjoe@gmail.com>
2020-12-08 13:05:11 -0800

    header done bg image added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,46 +1,58 @@
-import React from 'react'
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-    Typography,
-    Avatar,
-    Grid,
-    Box,
-} from "@material-ui/core";
-import Typed from 'react-typed';
-import coolpic from '../coolpic.jpg';
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
+import Typed from "react-typed";
+import coolpic from "../coolpic.jpg";
+import zIndex from "@material-ui/core/styles/zIndex";
 
 //CSS Styles
-const useStyles = makeStyles(theme=> ({
-    avatar: {
-        width: theme.spacing(15),
-        height: theme.spacing(15),
-        margin: theme.spacing(1),
-    },
-    title: {
-        
-    }
-}))
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(30),
+    height: theme.spacing(30),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: "tomato",
+  },
+  subtitle: {
+    color: "tan",
+    marginBottom: "3rem",
+  },
+  typedContainer: {
+    background: "rgba(38,50,56,0.5)",
+    borderRadius: 5,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate( -50%, -50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1,
+  },
+}));
 
 const Header = () => {
-    const classes = useStyles()
-    return (
-        <Box>
-            <Avatar className={classes.avatar} src={coolpic} alt="pic of me"/>
-            <Typography className={classes.title} variant="h4">
-                <Typed strings={['Joe Schlick']} 
-                typeSpeed={40} 
-                />
-            </Typography>
-            <br/>
-            <Typography className={classes.subtitle} variant="h5">
-                <Typed strings={['Web Design', 'Web Development', 'MERN Stack']} 
-                typeSpeed={40} 
-                backSpeed={60}
-                loop
-                />
-            </Typography>
-        </Box>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Box className={classes.typedContainer}>
+      <Grid container justify="center">
+        <Avatar className={classes.avatar} src={coolpic} alt="pic of me" />
+      </Grid>
+      <Typography className={classes.title} variant="h4">
+        <Typed strings={["Joe Schlick"]} typeSpeed={40} />
+      </Typography>
+      <br />
+      <Typography className={classes.subtitle} variant="h5">
+        <Typed
+          strings={["Web Design", "Web Development", "MERN Stack"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+      </Typography>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
