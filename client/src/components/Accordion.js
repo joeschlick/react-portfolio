a74c851f7286d1a19fd82ef73eac90463ec0480f
slowdive <schlickjoe@gmail.com>
2021-02-06 14:29:49 -0800

    accordion button styling sorted

--- client/src/components/Accordion.js
+++ client/src/components/Accordion.js
@@ -1,12 +1,34 @@
 import React from "react";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 
 import "./Accordion.css";
 
+// const useStyles = makeStyles((theme) => ({
+//   title: {
+//     color: "white",
+//     borderColor: "white",
+//     textAlign: "left",
+//     fontSize: "24px",
+//     margin: "20px"
+//   },
+//   accordionSection: {
+//     display: "flex",
+//     flexDirection: "column",
+    
+//   }
+
+// }))
+
 function Accordion(props) {
+  // const classes = useStyles()
  return (
    <div className="accordion__section">
-     <button className="accordion">
-       <p className="accordion__title">{props.title}</p>
+     <button variant="outlined" className="accordion">
+       <p  className="accordion__title">{props.title}
+         </p>
+       <ChevronLeftIcon />
      </button>
      <div className="accordion__content">
        <div
