48273c85874b07e22c59c827bac144240edae6ba
slowdive <schlickjoe@gmail.com>
2021-02-06 16:13:02 -0800

    accordions for resume complete

--- client/src/components/Accordion.js
+++ client/src/components/Accordion.js
@@ -1,43 +1,45 @@
 import React from "react";
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { useState } from "react";
+import { useRef } from "react";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { Button, Typography } from "@material-ui/core";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 
 import "./Accordion.css";
 
-// const useStyles = makeStyles((theme) => ({
-//   title: {
-//     color: "white",
-//     borderColor: "white",
-//     textAlign: "left",
-//     fontSize: "24px",
-//     margin: "20px"
-//   },
-//   accordionSection: {
-//     display: "flex",
-//     flexDirection: "column",
-    
-//   }
-
-// }))
-
 function Accordion(props) {
-  // const classes = useStyles()
- return (
-   <div className="accordion__section">
-     <button variant="outlined" className="accordion">
-       <p  className="accordion__title">{props.title}
-         </p>
-       <ChevronLeftIcon fontSize="large" className="accordion__icon"/>
-     </button>
-     <div className="accordion__content">
-       <div
-         className="accordion__text"
-         dangerouslySetInnerHTML={{ __html: props.content }}
-       />
-     </div>
-   </div>
- );
+  const [setActive, setActiveState] = useState("");
+  const [setHeight, setHeightState] = useState("0px");
+  const [setRotate, setRotateState] = useState("accordion__icon")
+
+  const content = useRef(null);
+
+  function toggleAccordion() {
+    setActiveState(setActive === "" ? "active" : "");
+    setHeightState(
+      setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+    );
+    setRotateState(setActive === "active" ? "accordion__icon" : "accordion__icon rotate");
+  }
+
+  return (
+    <div className="accordion__section">
+      <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
+        <p className="accordion__title">{props.title}</p>
+        <ChevronLeftIcon fontSize="large" className={`${setRotate}`} />
+      </button>
+      <div
+        ref={content}
+        style={{ maxHeight: `${setHeight}` }}
+        className="accordion__content"
+      >
+        <div
+          className="accordion__text"
+          dangerouslySetInnerHTML={{ __html: props.content }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Accordion;
